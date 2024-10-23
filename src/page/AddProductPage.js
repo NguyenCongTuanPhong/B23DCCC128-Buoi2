d83a74aa158cdeaab949e28eae9245541212c56f
NguyenCongTuanPhong <mrphong2k5@gmail.com>
2024-10-23 11:16:29 +0700

    abcxyz

---
+++ src/page/AddProductPage.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import AddProductForm from "../components/AddProduct";
+
+const AddProductPage = () => {
+  return (
+    <div>
+      <AddProductForm/>
+    </div>
+  );
+};
+
+export default AddProductPage;
---
+++ src/page/EditProductPage.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import EditProduct from "../components/EditProduct";
+
+const EditProductPage = () => {
+  return (
+    <div>
+      <EditProduct />
+    </div>
+  );
+};
+
+export default EditProductPage;
